e0e9b7592fed7552d76f38dbdf5dafe329287b0d
Anton Cheng <antonassocareer@gmail.com>
2021-05-16 17:16:52 +0800

    udpate: favicon

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -11,7 +11,7 @@ function Home() {
       <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
         <Header primary="Hodl!" />
         <Comment padding={0} text="Welcome to Hodl." />
-        Here we help you hodl your coins, and make extra money by holding longer than anyone else.
+        Here we help you hodl your coins, and make extra money by holding longer than everyone else.
         <img style={{ paddingTop: '3%' }} src={holding} height={150} alt="logo-beer"></img>
         <Comment padding={0} text="Salud!" />
       </div>
