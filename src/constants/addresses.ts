584c6055f52e61a1e196866eefbe935e65c9fd31
Anton Cheng <antonassocareer@gmail.com>
2021-06-27 21:12:06 +0800

    misc: update factory address

--- src/constants/addresses.ts
+++ src/constants/addresses.ts
@@ -5,6 +5,6 @@ export const ZERO_ADDR = '0x0000000000000000000000000000000000000000'
 export const factories: {
   [key in SupportedNetworks]: string
 } = {
-  [SupportedNetworks.Ropsten]: '0xE4c2981f17102390C694698504741D8a6325e4f2',
-  [SupportedNetworks.Kovan]: '0x0bced6E2D5548c89882686fBBdd5D8BdFF44827d',
+  [SupportedNetworks.Ropsten]: '0x05c62EeFa1e7c2559a9CE59f149514d5334C799E',
+  [SupportedNetworks.Kovan]: '0x0ecfE8Ed77b7483EC1E4c8ceF98B97BF151B4a4b',
 }
